2291d7cf235b4dbce00a8ede3c0be38d1a51c549
Tony Lopez <tony.lopez1013@gmail.com>
2014-11-20 21:04:07 -0600

    Updating Order View
    - Removed "Wizard" functionality (css/js)
    - Added Select for time selection (per Adam's request)
    - Double checked responsive flow (bootstrap grid elems)
    - Added .gitignore file (for eclipse settings)

--- js/home.js
+++ js/home.js
@@ -13,22 +13,4 @@ $(function() {
 		$(".nav .dropdown").show();
 		$("#login_dropdown").hide();
 	});
-
-	$(".order_panel .timeSelection, #new_order_submit_order_info").click(function() {
-		var $currentOrderPanel = $(".order_panel:visible");
-		$currentOrderPanel.hide();
-		$currentOrderPanel.next().show();
-		
-		$("#new_order_go_back").show();
-	});
-
-	$("#new_order_go_back").click(function() {
-		var $currentOrderPanel = $(".order_panel:visible");
-		$currentOrderPanel.hide();
-		var $previousOrderPanel = $currentOrderPanel.prev().show();
-		
-		if (parseInt($previousOrderPanel.attr("id").replace("order_panel_", "")) === 1) {
-			$(this).hide();
-		}
-	});
 });
